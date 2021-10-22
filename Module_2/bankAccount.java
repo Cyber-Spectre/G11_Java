618c3b4e125f27294bd3db77e9ef10d2faa211f6
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2021-10-22 10:28:07 -0600

    Add files via upload

---
+++ Module_2/bankAccount.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Module_2;
+
+/**
+ *
+ * @author 105337005
+ */
+public class bankAccount {
+    //data variables
+    private static int accountNum = 1000000;
+    private String username;
+    private double balance = 00.00;
+    private double annualInterest;
+    private double withdrawFee;
+    
+    public bankAccount() {
+        accountNum++;   
+    }
+
+}
