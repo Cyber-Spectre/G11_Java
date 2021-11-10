4c7b2e912b2cf8f637caa23acd94afea24be87e6
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2021-11-10 09:43:18 -0700

    Add files via upload

--- Module_3_Arrays/Assignment2.java
+++ Module_3_Arrays/Assignment2.java
@@ -25,7 +25,7 @@ public class Assignment2 {
             System.out.println("Please select an option: \n"
                 + "0: Exit program \n"
                 + "1: Populate the array randomly \n"
-                + "2: Display array"
+                + "2: Display array \n"
                 + "3: Find 3x3 array with highest sum \n");
 
             int input = scan1.nextInt();
@@ -54,19 +54,23 @@ public class Assignment2 {
                 
                 case 2:
                     
-                    for (int x = 1; x < array.length; x++) {
+                    System.out.println("YX || X1 | X2 | X3 | X4 | X5 | X6 | X7 | X8 | X9 | X10 | X11 | X12 | X13 | X14 | X15 | X16 | X17 | X18 | X19 | X20");
+                    
+                    for (int y = 1; y < array.length; y++) {
                         
-                        System.out.println("X" + x + " || " + array[x][1] + " | " 
-                                + array[x][2] + " | " + array[x][2] + " | " + array[x][4] 
-                                + " | " + array[x][5] + " | " + array[x][6] + " | " 
-                                + array[x][7] + " | " + array[x][8] + " | " + array[x][9] 
-                                + " | " + array[x][10] + " | " + array[x][11] + " | " 
-                                + array[x][12] + " | " + array[x][13] + " | " + array[x][14]
-                                + " | " + array[x][15] + " | " + array[x][16] + " | " 
-                                + array[x][17] + " | " + array[x][18] + " | " + array[x][19]
-                                + " | " + array[x][20] + "\n");
+                        System.out.println("Y" + y + " || " + array[1][y] + " | " 
+                                + array[2][y] + " | " + array[y][2] + " | " + array[y][4] 
+                                + " | " + array[5][y] + " | " + array[6][y] + " | " 
+                                + array[7][y] + " | " + array[8][y] + " | " + array[9][y] 
+                                + " | " + array[10][y] + " | " + array[11][y] + " | " 
+                                + array[12][y] + " | " + array[13][y] + " | " + array[14][y]
+                                + " | " + array[15][y] + " | " + array[16][y] + " | " 
+                                + array[17][y] + " | " + array[18][y] + " | " + array[19][y]
+                                + " | " + array[20][y]);
                         
                     }
+                    
+                    System.out.println("\n");
                     
                 break;
                 
