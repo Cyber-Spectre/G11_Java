ee7ec1d532725c0967edd53430bceb214f1219b3
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2021-11-10 10:42:26 -0700

    Add files via upload

---
+++ Module_3_Arrays/Mastery1.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Module_3_Arrays;
+
+/**
+ *
+ * @author 105337005
+ */
+public class Mastery1 {
+    
+    public static void main(String[] args) {
+        
+        int [] array = new int[200001];
+        boolean check = false;
+        for (int x = 2; x < array.length; x++) {
+            
+            array[x] = x;
+            
+        }
+        
+        for (int x = 2; x < array.length; x++) {
+            
+            check = false;
+            
+            for (int y = 2; y < x; y++) {
+            
+                if (array[x] % y == 0) {
+                    check = true;
+                }
+            
+            }
+            
+            if(!check) {
+                System.out.println(array[x]);
+            }
+        }
+        
+    }
+    
+}
