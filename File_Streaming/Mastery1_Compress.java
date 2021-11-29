2768142b6bc975d14186c17ddc990d103e895318
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2021-11-29 10:14:45 -0700

    Add files via upload

--- File_Streaming/Mastery1_Compress.java
+++ File_Streaming/Mastery1_Compress.java
@@ -12,6 +12,7 @@ package File_Streaming;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.FileWriter;
 import java.util.Scanner;
 public class Mastery1_Compress {
     
@@ -46,6 +47,67 @@ public class Mastery1_Compress {
         catch(FileNotFoundException f) {
             System.out.println("An error occurred.");
             f.printStackTrace();
+        }
+        
+        int dataLen = dataStore1.length();
+        int compVar = -1;
+        int compHold = 1;
+        
+        for(int x = 0; x < dataLen; x++) {
+            
+            if(compVar == -1) {
+                
+                if(dataStore1.charAt(x) == '1') {
+                    compVar = 1;
+                    x++;
+                }
+                else if(dataStore1.charAt(x) == '0') {
+                    compVar = 0;
+                    x++;
+                }
+            }
+            else if (compVar == 1) {
+               if(dataStore1.charAt(x) == '1') {
+                   compHold++;
+                   x++;
+               }
+               else {
+                   dataStore2 = (dataStore2 + compHold + " 1 ");
+                   compHold = 1;
+                   compVar = 0;
+                   x++;
+               }
+            }
+            else if (compVar == 0) {
+               if(dataStore1.charAt(x) == '0') {
+                   compHold++;
+                   x++;
+               }
+               else {
+                   dataStore2 = (dataStore2 + compHold + " 0 ");
+                   compHold = 1;
+                   compVar = 1;
+                   x++;
+               }
+            }
+            else {
+                System.out.println("An error occurred.");
+            }
+        }
+        
+        if(compHold > 0) {
+            dataStore2 = dataStore2 = (dataStore2 + compHold + " " + compVar + " ");
+        }
+        
+        try {   
+        FileWriter myWriter = new FileWriter("C:\\Users\\105337005\\Documents\\NetBeansProjects\\G11_Java\\src\\main\\java\\File_Streaming\\dataCompress.txt");
+        myWriter.write(dataStore2);
+        myWriter.close();
+            System.out.println("Successfully wrote to file.");
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
     }
 }
