821baf77015376e9a80fbd03669030ff7ec9d6ab
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2021-12-06 10:46:11 -0700

    Add files via upload

--- Final_Project/glue.java
+++ Final_Project/glue.java
@@ -5,13 +5,22 @@
  */
 package Final_Project;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author 105337005
  */
 public class glue {
     
+    public static Scanner scan1 = new Scanner(System.in);
+    public static Random rand = new Random();
+    
+    private String path = "C:\\Users\\105337005\\Documents\\NetBeansProjects\\G11_Java\\src\\main\\java\\Final_Project\\";
+    
     public static void main(String[] args) {
+        
         
     }
     
