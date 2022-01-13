577f5876668cb362142de65ee7f1b6099dfcc4b4
Spectre <52917568+Cyber-Spectre@users.noreply.github.com>
2022-01-13 12:09:57 -0700

    Add files via upload

--- Final_Project/glue.java
+++ Final_Project/glue.java
@@ -28,12 +28,17 @@ public class glue {
     public static int weapID;
     public static int roomID;
     
+    public static String [] charNames = new String[6];
+    public static String [] weapNames = new String[6];
+    public static String [] roomNames = new String[9];
+    
     public static int [] weapLoc = new int[9];
     public static int locHold;
     public static boolean [] locCheck = new boolean[9];
     
-    public static boolean [] isSearched = new boolean[9];
-    public static String [] isSearchedStr = new String[9];
+    public static boolean [] roomSearched = new boolean[9];
+    public static String [] roomSearchedStr = new String[9];
+    public static boolean [] weapSearched = new boolean[6];
     
     public static int turn;
     public static int turns = 20;
@@ -42,9 +47,7 @@ public class glue {
     
     public static int chance;
     
-    public static int inputInt;
-    public static String inputStr;
-    
+    public static int input;
     
     public static boolean whilePrint;
     public static boolean whileChoice;
@@ -83,9 +86,9 @@ public class glue {
             + "2) Quit Game \n");
         
         scan1 = new Scanner(System.in);
-        inputInt = scan1.nextInt();
+        input = scan1.nextInt();
         
-        switch(inputInt) {
+        switch(input) {
             case 1:
                 Startup();
             break;
@@ -99,10 +102,10 @@ public class glue {
     public static void Startup() {
         for (int x = 0; x < 9; x++) {
             locCheck[x] = false;
-            isSearched[x] = false;
+            roomSearched[x] = false;
+            weapSearched[x] = false;
         }
         
-        String [] charNames = new String[6];
         for (int x = 0; x < 6; x++) { //sets character name displays
             charNames[x] = "";
             whilePrint = true;
@@ -117,7 +120,6 @@ public class glue {
             }
         }
         
-        String [] roomNames = new String[9];
         for (int x = 0; x < 9; x++) { //sets room name displays
             roomNames[x] = "";
             whilePrint = true;
@@ -125,6 +127,20 @@ public class glue {
             for (int y = 0; whilePrint; y++) {
                 if(arrRooms[x].charAt(y) != '|') {
                     roomNames[x] = roomNames[x] + arrRooms[x].charAt(y);
+                }
+                else {
+                    whilePrint = false;
+                }
+            }
+        }
+        
+        for (int x = 0; x < 6; x++) { //sets weapon name displays
+            weapNames[x] = "";
+            whilePrint = true;
+            scan1 = new Scanner(arrWeaps[x]);
+            for (int y = 0; whilePrint; y++) {
+                if(arrWeaps[x].charAt(y) != '|') {
+                    weapNames[x] = weapNames[x] + arrWeaps[x].charAt(y);
                 }
                 else {
                     whilePrint = false;
@@ -180,8 +196,8 @@ public class glue {
         
         while (!isReady) {
             scan1 = new Scanner(System.in);
-            inputInt = scan1.nextInt();
-            if (inputInt == 1) {
+            input = scan1.nextInt();
+            if (input == 1) {
                 isReady = true;
                 turn = 1;
                 RoundStart();
@@ -192,54 +208,84 @@ public class glue {
     public static void RoundStart() {
         
         System.out.println("-----------------------------------------------------------------\n"
-            + "Turn " + turn + " of " + turns + "\n"
-            + "Choose how hard you want to roll your dice: (1-10)");
+            + "Turn " + turn + " of " + turns);
+        if (d4) {
+            System.out.println("You have a d4 this turn, so you can only roll a 1-4\n"
+                + "Choose how hard you want to roll (1-10)");
+            
+            isReady = false;
         
-        isReady = false;
-        
-        while (!isReady) {
-            scan1 = new Scanner(System.in);
-            String input = scan1.nextLine();
-            try {
-                if (Integer.valueOf(input) > 0 && Integer.valueOf(input) < 11) {
-                    for (int x = 0; x < Integer.valueOf(input); x++) {
-                        dice = rand.nextInt(6) + 1;
+            while (!isReady) {
+                scan1 = new Scanner(System.in);
+                String input = scan1.nextLine();
+                try {
+                    if (Integer.valueOf(input) > 0 && Integer.valueOf(input) < 11) {
+                        for (int x = 0; x < Integer.valueOf(input); x++) {
+                            dice = rand.nextInt(4) + 1;
+                        }
+                        isReady = true;
                     }
-                    isReady = true;
+                    else {
+                        System.out.println("Number out of range");
+                    }
                 }
-                else {
-                    System.out.println("Number out of range");
+                catch (Exception e) {
+                    System.out.println("Input a number");
                 }
             }
-            catch (Exception e) {
-                System.out.println("Input a number");
+
+            System.out.println("You rolled a " + dice);
+        }
+        else {
+            System.out.println("Choose how hard you want to roll (1-10)");
+            
+            isReady = false;
+        
+            while (!isReady) {
+                scan1 = new Scanner(System.in);
+                String input = scan1.nextLine();
+                try {
+                    if (Integer.valueOf(input) > 0 && Integer.valueOf(input) < 11) {
+                        for (int x = 0; x < Integer.valueOf(input); x++) {
+                            dice = rand.nextInt(6) + 1;
+                        }
+                        isReady = true;
+                    }
+                    else {
+                        System.out.println("Number out of range");
+                    }
+                }
+                catch (Exception e) {
+                    System.out.println("Input a number");
+                }
             }
+
+            System.out.println("You rolled a " + dice);
         }
         
-        System.out.println("You rolled a " + dice);
+        
         
         System.out.println("-----------------------------------------------------------------\n"
             + "Please select an action:\n"
             + "1) +Search a room \n"
             + "2) +Question suspect\n"
             + "3) +C H E E S E\n"
-            + "4) +Guess killer\n");
+            + "4) +Guess killer");
         
         if (dice == 1) {
             System.out.println("5) +Lucky roll\n");
         }
         
         scan1 = new Scanner(System.in);
-        inputInt = scan1.nextInt();
+        input = scan1.nextInt();
         
         whileChoice = false;
         
         while (!whileChoice) {
             try {
-                switch(inputInt) {
+                switch(input) {
                     case 1:
-                        
-
+                        SearchStart();
                         break;
                     case 2:
 
@@ -259,6 +305,7 @@ public class glue {
                         else {
                         break;
                         }
+                        break;
                 }
             }
             catch (Exception e) {
@@ -268,7 +315,42 @@ public class glue {
     }
     
     public static void RoundMain() {
+        System.out.println("-----------------------------------------------------------------\n"
+            + "Turn " + turn + " of " + turns + "\n"
+            + "You have " + dice + " moves left\n"
+            + "\n"
+            + "1) +Search a room\n"
+            + "2) +Question suspect\n"
+            + "3) +C H E E S E\n"
+            + "4) +Guess killer");
         
+        scan1 = new Scanner(System.in);
+        input = scan1.nextInt();
+        
+        whileChoice = false;
+        
+        while (!whileChoice) {
+            try {
+                switch(input) {
+                    case 1:
+                        
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+                        whileChoice = true;
+                        Cheese();
+                        break;
+                    case 4:
+                        
+                        break;
+                }
+            }
+            catch (Exception e) {
+                System.out.println("Invalid input");
+            }
+        }
     }
     
     public static void Rooms() {
@@ -288,34 +370,96 @@ public class glue {
             + "5% chance of it being blue cheese and you dying (lose the game instantly)\n"
             + "\n"
             + "Do you eat the cheese?\n"
-                    + "1) -Yes\n"
-                    + "2) +No\n");
+            + "1) -Yes\n"
+            + "2) +No\n");
         
         scan1 = new Scanner(System.in);
-        inputInt = scan1.nextInt();
+        input = scan1.nextInt();
         
-        if (inputInt == 1) {
+        if (input == 1) {
             dice--;
             chance = rand.nextInt(100) + 1;
             if (chance <= 70) {
                 System.out.println("Nothing happened.");
+                RoundMain();
             }
             else if (chance <= 90) {
                 System.out.println("The cheese gave you more moves!");
                 dice = dice + 3;
+                RoundMain();
             }
             else if (chance <= 95) {
                 System.out.println("The cheese gave you more turns!!");
                 turns = turns + 2;
+                RoundMain();
             }
             else  {
                 System.out.println("It was blue cheese, you died!!!");
                 System.exit(0);
             }
         }
-        else if (inputInt == 2) {
+        else if (input == 2) {
             RoundMain();
         }
+    }
+    
+    public static void SearchStart() {
+        System.out.println("What room would you like to search?\n"
+            + "1) -" + roomNames[0] + "\n"
+            + "2) -" + roomNames[1] + "\n"
+            + "3) -" + roomNames[2] + "\n"
+            + "4) -" + roomNames[3] + "\n"
+            + "5) -" + roomNames[4] + "\n"
+            + "6) -" + roomNames[5] + "\n"
+            + "7) -" + roomNames[6] + "\n"
+            + "8) -" + roomNames[7] + "\n"
+            + "9) -" + roomNames[8] + "\n");
+        
+        scan1 = new Scanner(System.in);
+        input = scan1.nextInt();
+        
+        switch(input) {
+            case 1:
+                SearchRoom(0);
+                break;
+            case 2:
+                SearchRoom(1);
+                break;
+            case 3:
+                SearchRoom(2);
+                break;
+            case 4:
+                SearchRoom(3);
+                break;
+            case 5:
+                SearchRoom(4);
+                break;
+            case 6:
+                SearchRoom(5);
+                break;
+            case 7:
+                SearchRoom(6);
+                break;
+            case 8:
+                SearchRoom(7);
+                break;
+            case 9:
+                SearchRoom(8);
+                break;
+        }
+    }
+    
+    public static void SearchRoom(int input) {
+        
+        roomSearched[input] = true;
+        
+        System.out.println("-----------------------------------------------------------------\n"
+            + "Room " + roomNames[input] + "\n"
+            + "\n"
+            + "What do you want to do in this room?\n"
+            + "0) -Exit room\n"
+                    + "1)Inspect room\n"
+                    + "");
     }
     
     public static void LuckyRoll() {
@@ -334,39 +478,39 @@ public class glue {
             + "2) +No\n");
         
         scan1 = new Scanner(System.in);
-        inputInt = scan1.nextInt();
+        input = scan1.nextInt();
         
         whileChoice = false;
         
         while (!whileChoice) {
             try {
-                switch(inputInt) {
+                switch(input) {
                     case 1:
                         dice--;
                         int lucky1 = rand.nextInt(2) + 1;
-                        String lucky2;
+                        int lucky2;
                         int luckyG1 = 0;
-                        String luckyG2 = "";
+                        int luckyG2 = 0;
                         
                         chance = rand.nextInt(2);
                         
                         if (chance == 0) {
-                            lucky2 = "A";
+                            lucky2 = 3;
                         }
                         else {
-                            lucky2 = "B";
+                            lucky2 = 4;
                         }
                         
                         whileChoice = false;
                         System.out.println("Choose either 1 or 2:");
                         while (!whileChoice) {
                             scan1 = new Scanner(System.in);
-                            inputInt = scan1.nextInt();
+                            input = scan1.nextInt();
                             
                             try {
-                                if (inputInt == 1 || inputInt == 2) {
+                                if (input == 1 || input == 2) {
                                     whileChoice = true;
-                                    luckyG1 = inputInt;
+                                    luckyG1 = input;
                                 }
                                 else {
                                     System.out.println("Invalid input");
@@ -378,18 +522,14 @@ public class glue {
                         }
                         
                         whileChoice = false;
-                        System.out.println("Choose either A or B:");
+                        System.out.println("Choose either 3 or 4:");
                         while (!whileChoice) {
                             scan1 = new Scanner(System.in);
-                            inputStr = scan1.nextLine();
+                            input = scan1.nextInt();
                             
-                            if (inputStr == "A" || inputStr == "a") {
+                            if (input == 3 || input == 4) {
                                 whileChoice = true;
-                                luckyG2 = "A";
-                            }
-                            else if (inputStr == "B" || inputStr == "b") {
-                                whileChoice = true;
-                                luckyG2 = "B";
+                                luckyG2 = input;
                             }
                             else {
                                 System.out.println("Invalid input");
@@ -406,16 +546,61 @@ public class glue {
                         }
                         
                         if (luckyScore == 2) {
+                            System.out.println("You got both right! choose how hard you want to reroll your dice (1-10");
                             
+                            while (!isReady) {
+                                scan1 = new Scanner(System.in);
+                                String input = scan1.nextLine();
+                                try {
+                                    if (Integer.valueOf(input) > 0 && Integer.valueOf(input) < 11) {
+                                        for (int x = 0; x < Integer.valueOf(input); x++) {
+                                            int reroll1 = rand.nextInt(6) + 1;
+                                            int reroll2 = rand.nextInt(6) + 1;
+                                            dice = reroll1 + reroll2;
+                                        }
+                                        isReady = true;
+                                        System.out.println("You rerolled a " + dice);
+                                        RoundMain();
+                                    }
+                                    else {
+                                        System.out.println("Number out of range");
+                                    }
+                                }
+                                catch (Exception e) {
+                                    System.out.println("Input a number");
+                                }
+                            }
                         }
                         else if (luckyScore == 1) {
+                            System.out.println("You got one right, choose how hard you want to reroll your dice (1-10)");
                             
+                            while (!isReady) {
+                                scan1 = new Scanner(System.in);
+                                String input = scan1.nextLine();
+                                try {
+                                    if (Integer.valueOf(input) > 0 && Integer.valueOf(input) < 11) {
+                                        for (int x = 0; x < Integer.valueOf(input); x++) {
+                                            int reroll1 = rand.nextInt(6) + 1;
+                                            dice = reroll1;
+                                        }
+                                        isReady = true;
+                                        System.out.println("You rerolled a " + dice);
+                                        RoundMain();
+                                    }
+                                    else {
+                                        System.out.println("Number out of range");
+                                    }
+                                }
+                                catch (Exception e) {
+                                    System.out.println("Input a number");
+                                }
+                            }
                         }
                         else {
                             System.out.println("You got neither right! Better luck next time");
                             turns = turns - 2;
                             d4 = true;
-                            
+                            RoundStart();
                         }
                         
                         break;
